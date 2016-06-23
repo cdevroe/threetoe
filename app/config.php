89307b215e632b5c8aaf278746025fdaf4e295a5
Colin Devroe <colin@cdevroe.com>
2016-06-23 13:41:24 -0400

    Removed Twilio information from config. Created private config.

--- app/config.php
+++ app/config.php
@@ -3,7 +3,4 @@
 $app_version =        '0.1.0';
 $data_location =      '/Users/cdevroe/Sites/clients/timbethel/threetoe/responses/';
 
-$twilio_sid = "ACXXXXXX"; // Your Account SID from www.twilio.com/user/account
-$twilio_token = "YYYYYY"; // Your Auth Token from www.twilio.com/user/account
-
 ?>
